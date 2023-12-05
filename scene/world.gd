9fc007cdff205337053b95380411404ec118ead0
borninachenil <82093897+borninachenil@users.noreply.github.com>
2023-12-05 02:13:47 +0100

    adaptation Random + 2/3 trucs
    nouvel objet "rock" et essai de créer la fonction de pathfinding mais encore infonctionnelle

--- scene/world.gd
+++ scene/world.gd
@@ -1,24 +1,24 @@
 extends Node2D
 
 # Array of the object scenes (PackedScene) to spawn
-var objects_to_spawn = [preload("res://Objet1.tscn"),preload("res://Objet2.tscn"),preload("res://Objet3.tscn")]
+var objects_to_spawn = [preload("res://scene/stick.tscn"),preload("res://scene/stick.tscn"),preload("res://scene/stick.tscn"),preload("res://scene/stick.tscn"),preload("res://scene/stick.tscn"),preload("res://scene/stick.tscn"),preload("res://scene/rock.tscn"),preload("res://scene/rock.tscn"),preload("res://scene/rock.tscn"),preload("res://scene/rock.tscn"),preload("res://scene/rock.tscn")]
 
 
 # Predefined positions
- # var positions = [[-1, -19], [3, -14], [-3, -11], [3, -9], [7, -10], [7, -7], [2, -5], [-3, -5], [6, 5], [6, 14]]
-var positions = [[50, 10], [90, 70], [30, 10]]
+# var positions = [[-1, -19], [3, -14], [-3, -11], [3, -9], [7, -10], [7, -7], [2, -5], [-3, -5], [6, 5], [6, 14]]
+var positions = [[182, 145], [111, 189], [30, 10],[111, 189],[-91, 186],[-174, 137],[-190, 35]
+,[-99, 36],[-170, -58],[8, -67],[151, -63],[299, -113],[436, -147],[-381, -124],[76, 301],[-128, 282],[378, 142]]
 func _ready():
 	randomize()
 	shuffle_positions()
 	spawn_objects()
-	var viewport_size = get_viewport_rect().size
 func shuffle_positions():
 	positions.shuffle()
 
 func spawn_objects():
 	for i in range(min(objects_to_spawn.size(), positions.size())):
-		var object_scene = objects_to_spawn[0]
-		var object_instance = object_scene.instance()
+		var object_scene = objects_to_spawn[i]
+		var object_instance = object_scene.instantiate()
 
 		
 		object_instance.global_position = Vector2(positions[i][0], positions[i][1])
--- script/Player2.gd
+++ script/Player2.gd
@@ -3,26 +3,48 @@ extends CharacterBody2D
 var speed = 100
 var player_state
 var score = 0
-
+var seekable_group = "objets"
+var navigation_agent: NavigationAgent2D
 @export var inventory : Inventory
 
-func _physics_process(delta):
-	var direction = Input.get_vector("flecheGauche", "flecheDroite", "flecheHaut", "flecheBas")
+
+func _ready():
 	
-	if direction.x == 0 and direction.y == 0 : 
-		player_state = "idle"
-		
-	elif direction.x != 0 or direction.y != 0 : 
-		player_state = "walking"
-	
-	velocity = direction * speed
-	move_and_slide()
+	navigation_agent = get_node("NavigationAgent2D")
+
+
+func _process(delta):
+	var closest_item = get_closest_item()
+	if closest_item:
+		navigation_agent.set_target_location(closest_item.global_position)
+		move_towards_target(delta)	
+
 
 
 func player() : 
 	pass
 
-	
+
+func get_closest_item():
+	var items = get_tree().get_nodes_in_group(seekable_group)
+	var min_distance = INF
+	var closest_item = null
+
+	for item in items:
+		var distance = global_position.distance_to(item.global_position)
+		if distance < min_distance:
+			min_distance = distance
+			closest_item = item
+
+	return closest_item
+
+func move_towards_target(delta):
+		var next_position = navigation_agent.get_next_location()
+		var direction = (next_position - global_position).normalized()
+		var velocity = direction*speed
+		move_and_slide()
+
+
 func collect(item) : 
 	# si l'inventaire a encore une case vide alors on met l'objet dedans
 	#if(inventory.items.has(null)): 
